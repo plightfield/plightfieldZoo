c580bfb30c81553f9f5bf82926497bd11126ac0f
plightfield <1207120484@qq.com>
2020-04-15 10:21:12 +0800

    change

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
       {
         repoUrl: "https://www.yuque.com/ze0bka/fn",
         authToken: "JM0YNWXGYjXRyny6vNljG8WNPjNMMbVCSHSNdAXd",
+        html: true,
       },
     ],
   ],
